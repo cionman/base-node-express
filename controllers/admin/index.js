45dac94d93186922e1e19f227f9e3cb6b89a155e
cionman <topspin1278@naver.com>
2020-10-02 11:56:19 +0900

    ADD - 컨트롤러 구조 변경 및 adminRequired 미들웨어 pseudo code 추가

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,26 +1,36 @@
-const { Router } = require('express');
+const {Router} = require('express');
 const router = Router();
-const ctrl = require('./admin.ctrl');
 
-function testMiddleWare( req, res, next ){
+function testMiddleWare(req, res, next) {
     console.log('첫번째 미들웨어');
     next();
 }
 
-function testMiddleWare2( req, res, next ){
+function testMiddleWare2(req, res, next) {
     console.log('두번째 미들웨어');
     next();
 }
 
-router.get('/', testMiddleWare, testMiddleWare2 , (req,res) => {
+
+
+router.get('/', testMiddleWare, testMiddleWare2, (req, res) => {
     res.send('admin app');
 });
 
-router.get('/products', ctrl.get_products );
+router.get('/products', (_, res) => {
+    res.render('admin/products.html',
+        {message: "<h1>hello</h1><script>alert()</script>"} // message 란 변수를 템플릿으로 내보낸다.
+    );
+});
 
-router.get('/products/write', ctrl.get_products_write );
+router.get('/products/write', (_, res) => {
+    res.render('admin/write.html');
+});
 
-router.post('/products/write', ctrl.post_products_write );
+router.post('/products/write', (req, res) => {
+        res.send(req.body);
+    }
+);
 
 module.exports = router;
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,11 @@
 const { Router } = require('express');
 const router = Router()
 
-router.use('/admin', require('./admin'));
+function adminRequired(req, res, next) {
+    console.error('TODO : 관리자 로그인 체크 필요');
+    next();
+}
+
+router.use('/admin', adminRequired, require('./admin'));
 
 module.exports = router;
