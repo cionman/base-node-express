f7a9f2aa0bbc1f0588897a033596b64a5e59fe5d
cionman <topspin1278@naver.com>
2020-10-08 18:58:24 +0900

    ADD - 객체를 트리구조로 살펴볼수 있는 유틸함수 추가

--- common/util/func.js
+++ common/util/func.js
@@ -10,6 +10,12 @@ module.exports.wrapAsync = function (fn) {
     }
 }
 
+/**
+ * puppeter wrapper
+ *
+ * @param fn
+ * @returns {Promise<void>}
+ */
 module.exports.wrapPuppeteer = async function (fn) {
     // 브라우저 열기
     const browser = await puppeteer.launch({
@@ -26,3 +32,18 @@ module.exports.wrapPuppeteer = async function (fn) {
     // 브라우저 닫기
     await browser.close();
 }
+
+/**
+ * 객체 트리 구조 확인
+ * @param o
+ * @param m
+ * @param v
+ */
+module.exports.tree = (o,m=o,v='') => {
+    for(e in o){
+        if(typeof o!=='object'){console.log(v+'┗╸'+o);return}
+        Array.isArray(o)?!Array.isArray(o[e])?console.log(v+(o.length===1||o[e]===o[o.length-1]?'┗╸':'┣╸')+o[e]):Tree(o[e],m,v+(o.length===1||o[e]===o[o.length-1]?' '.repeat(o[e].toString.length+1):'┃'+' '.repeat(e.length))):(console.log(v+(e===Object.keys(m)[0]?'┏╸':Object.keys(o).length===1||Object.keys(o)[Object.keys(o).length-1]===e?'┗╸':'┣╸')+e),Tree(typeof o[e]!=='object'?String(o[e]):o[e],m,v+(Object.keys(o).length===1||Object.keys(o)[Object.keys(o).length-1]===e?' '.repeat(e.length+1):'┃'+' '.repeat(e.length))))
+    }
+}
+
+
