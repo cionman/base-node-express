d30789af28b897f9521e90bde4a585db737bae99
cionman <topspin1278@naver.com>
2020-10-09 19:36:16 +0900

    FIX - tree util 메소드 오류 수정

--- common/util/func.js
+++ common/util/func.js
@@ -33,17 +33,20 @@ module.exports.wrapPuppeteer = async function (fn) {
     await browser.close();
 }
 
+
+const tree  = (o,m=o,v='') => {
+    for(e in o){
+        if(typeof o!=='object'){console.log(v+'┗╸'+o);return}
+        Array.isArray(o)?!Array.isArray(o[e])?console.log(v+(o.length===1||o[e]===o[o.length-1]?'┗╸':'┣╸')+o[e]):tree(o[e],m,v+(o.length===1||o[e]===o[o.length-1]?' '.repeat(o[e].toString.length+1):'┃'+' '.repeat(e.length))):(console.log(v+(e===Object.keys(m)[0]?'┏╸':Object.keys(o).length===1||Object.keys(o)[Object.keys(o).length-1]===e?'┗╸':'┣╸')+e),tree(typeof o[e]!=='object'?String(o[e]):o[e],m,v+(Object.keys(o).length===1||Object.keys(o)[Object.keys(o).length-1]===e?' '.repeat(e.length+1):'┃'+' '.repeat(e.length))))
+    }
+}
+
 /**
  * 객체 트리 구조 확인
  * @param o
  * @param m
  * @param v
  */
-module.exports.tree = (o,m=o,v='') => {
-    for(e in o){
-        if(typeof o!=='object'){console.log(v+'┗╸'+o);return}
-        Array.isArray(o)?!Array.isArray(o[e])?console.log(v+(o.length===1||o[e]===o[o.length-1]?'┗╸':'┣╸')+o[e]):Tree(o[e],m,v+(o.length===1||o[e]===o[o.length-1]?' '.repeat(o[e].toString.length+1):'┃'+' '.repeat(e.length))):(console.log(v+(e===Object.keys(m)[0]?'┏╸':Object.keys(o).length===1||Object.keys(o)[Object.keys(o).length-1]===e?'┗╸':'┣╸')+e),Tree(typeof o[e]!=='object'?String(o[e]):o[e],m,v+(Object.keys(o).length===1||Object.keys(o)[Object.keys(o).length-1]===e?' '.repeat(e.length+1):'┃'+' '.repeat(e.length))))
-    }
-}
+module.exports.tree = tree
 
 
